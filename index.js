b67b08e5b6d9a1c5d5031f6c8af032e1547351fc
Ian Macalinao <ian@giza.us>
2014-09-20 16:05:58 -0500

    Make person have no songs by default -- they have to search

--- index.js
+++ index.js
@@ -16,21 +16,26 @@ function getRoom(roomId) {
     room = rooms[roomId] = {
       users: [],
       id: roomId,
-      songs: [{
-        id: 'asdf',
-        name: 'Recess',
-        artist: 'Skrillex',
-        artwork: 'http://upload.wikimedia.org/wikipedia/en/archive/5/52/20140314115000!RecessSkrillex.jpg',
-        votes: 0
-      }, {
-        id: 'asdff',
-        name: 'Play it Again',
-        artist: 'Luke Bryan',
-        artwork: 'http://tonefunk.com/wp-content/uploads/2014/03/UMG_cvrart_00602537511556_01_RGB72_1500x1500_13UAAIM59985.170x170-75.jpg',
-        votes: 0
-      }]
+      songs: []
     };
   }
+
+  if (false) {
+    room.songs = [{
+      id: 'asdf',
+      name: 'Recess',
+      artist: 'Skrillex',
+      artwork: 'http://upload.wikimedia.org/wikipedia/en/archive/5/52/20140314115000!RecessSkrillex.jpg',
+      votes: 0
+    }, {
+      id: 'asdff',
+      name: 'Play it Again',
+      artist: 'Luke Bryan',
+      artwork: 'http://tonefunk.com/wp-content/uploads/2014/03/UMG_cvrart_00602537511556_01_RGB72_1500x1500_13UAAIM59985.170x170-75.jpg',
+      votes: 0
+    }];
+  }
+
   room.count = _.filter(users, function(user) {
     return user.room === room.id;
   }).length;
