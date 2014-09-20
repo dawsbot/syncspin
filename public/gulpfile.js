77374987594a05888e9be1af8c3e1884a5bd205f
Ian Macalinao <ian@giza.us>
2014-09-19 22:04:37 -0500

    Add public directory

---
+++ public/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var inject = require('gulp-inject');
+
+gulp.task('inject', function() {
+  var sources = {
+    css: ['./bower_components/bootstrap/dist/css/bootstrap.min.css'],
+    js: [
+      './bower_components/jquery/dist/jquery.min.js',
+      './bower_components/angular/angular.min.js',
+      './bower_components/bootstrap/dist/js/bootstrap.min.js',
+      './bower_components/angular-ui-router/release/angular-ui-router.min.js'
+    ]
+  };
+
+  return gulp.src('./index.html')
+    .pipe(inject(gulp.src(sources.css)))
+    .pipe(inject(gulp.src(sources.js)))
+    .pipe(gulp.dest('./'));
+});
