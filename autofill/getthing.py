1e5d8839e2f93035a4a18756ab6ca6ac67f1a67d
dbotsford <dawsonbotsford@gmail.com>
2014-09-20 09:59:15 -0400

    Basic Dropdown maybe broken

--- autofill/getthing.py
+++ autofill/getthing.py
@@ -10,10 +10,12 @@ obj2 = myjson['data']['genres']
 
 #pp.pprint(obj2)
 for n in obj2:
-  outlist.append((n['display'] + ",").encode('utf-8').strip(',')) 
-  #print n['display']
+  outlist.append((n['display'] ).encode('utf-8').strip(',')) 
+  outlist.append((n['id'] + ",").encode('utf-8').strip(',')) 
+   
+#  print "n: " + str(n)
 print outlist
 
-f = open('people.txt', 'w')
-f.write(str(outlist))
+#f = open('people.txt', 'w')
+#f.write(str(outlist))
 
