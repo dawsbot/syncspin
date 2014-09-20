995a38dde6bdd59991df509f11926342bbd729c8
dbotsford <dawsonbotsford@gmail.com>
2014-09-19 23:16:30 -0400

    Python 2.7 compatible

--- BeatsAPI/APItest.py
+++ BeatsAPI/APItest.py
@@ -1,14 +1,12 @@
-from urllib import request
-import urllib
+import urllib2 
 import json
-import xml.etree.ElementTree as ET
+#import xml.etree.ElementTree as ET
 from pprint import pprint as pp
 
-resp = urllib.request.urlopen("https://partner.api.beatsmusic.com/v1/api/search?type=track&q=LCD+SoundSystem&client_id=mwfsu49zprp6rbcv4pfrux54")
+resp = urllib2.urlopen("https://partner.api.beatsmusic.com/v1/api/search?type=track&q=LCD+SoundSystem&client_id=mwfsu49zprp6rbcv4pfrux54")
 
-read = resp.read().decode('utf-8')
+jsonresp = json.loads(resp.read())
+pp(jsonresp)
 
-load = json.loads(read)
 
-pp(read)
 
