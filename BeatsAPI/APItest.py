460576ab03e9c88fc2a1e28b3d6fa0b946951071
dbotsford <dawsonbotsford@gmail.com>
2014-09-20 01:42:02 -0400

    login get figured out

--- BeatsAPI/APItest.py
+++
@@ -1,8 +0,0 @@
-import urllib2 
-import json
-from pprint import pprint as pp
-
-resp = urllib2.urlopen("https://partner.api.beatsmusic.com/v1/api/search?type=track&q=LCD+SoundSystem&client_id=ytuyn29p9e5b4udwtgwmughe")
-
-jsonresp = json.loads(resp.read())
-pp(jsonresp)
