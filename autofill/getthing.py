6bc0967bb784ed6e1762ddbf8801dd139277e921
dbotsford <dawsonbotsford@gmail.com>
2014-09-20 06:03:27 -0400

    autofill data bank ready

---
+++ autofill/getthing.py
@@ -0,0 +1,19 @@
+import json
+import pprint as pp
+
+outlist = []
+
+myfile = open("data1.json", "r").read()
+myjson = json.loads(myfile)
+
+obj2 = myjson['data']['genres']
+
+#pp.pprint(obj2)
+for n in obj2:
+  outlist.append((n['display'] + ",").encode('utf-8').strip(',')) 
+  #print n['display']
+print outlist
+
+f = open('people.txt', 'w')
+f.write(str(outlist))
+
