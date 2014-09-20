b29b2b2393df6ff29eb947e4d62ec427c6387e20
Ian Macalinao <ian@giza.us>
2014-09-20 02:18:30 -0500

    Add host view

--- public/app.js
+++ public/app.js
@@ -27,6 +27,17 @@ angular.module('syncspin', [
     };
   })
   .controller('HostCtrl', function($scope, $stateParams) {
+    $scope.songs = [{
+      name: 'Recess',
+      artist: 'Skrillex',
+      artwork: 'http://upload.wikimedia.org/wikipedia/en/archive/5/52/20140314115000!RecessSkrillex.jpg',
+      votes: 0
+    }, {
+      name: 'Play it Again',
+      artist: 'Luke Bryan',
+      artwork: 'http://tonefunk.com/wp-content/uploads/2014/03/UMG_cvrart_00602537511556_01_RGB72_1500x1500_13UAAIM59985.170x170-75.jpg',
+      votes: 0
+    }];
     $scope.roomId = $stateParams.roomId;
   })
   .controller('VoteCtrl', function($scope, $stateParams) {
