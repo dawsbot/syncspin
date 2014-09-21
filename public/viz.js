177470be243ae0c01da7f9a3e3425d075397c1b2
Tim Hyon <tim.c.hyon@gmail.com>
2014-09-20 22:42:35 -0700

    new viz

--- public/viz.js
+++ public/viz.js
@@ -1,31 +1,72 @@
-var vizNodes = [];
-socket.on('count', function(count) {
-    var vizNodes = count.nodes;
-        var width = 960,
-            height = 500;
-        var color = d3.scale.category20();
-        var force = d3.layout.force()
-            .charge(-120)
-            .linkDistance(30)
-            .size([width, height]);
-        var svg = d3.select("#dotViz").append("svg")
-            .attr("width", width)
-            .attr("height", height);
-        if (vizNodes){
-              force.nodes(vizNodes)
-                  .start();
-              var node = svg.selectAll(".node")
-                  .data(vizNodes)
-                .enter().append("circle")
-                  .attr("class", "node")
-                  .attr("r", 5)
-                  .style("fill", function(d) { return color(d.group); })
-                  .call(force.drag);
-              node.append("title")
-                  .text(function(d) { return d.name; });
-              force.on("tick", function() {
-                node.attr("cx", function(d) { return d.x; })
-                    .attr("cy", function(d) { return d.y; });
-              });
+socket.on('count', function(roomCount) {
+    var width = 960,
+        height = 700;
+
+    var nodes = d3.range(20).map(function() { return {radius: 5}; }),
+        root = nodes[0],
+        color = d3.scale.category10();
+
+    root.radius = 0;
+    root.fixed = true;
+
+    var force = d3.layout.force()
+        .gravity(0.05)
+        .charge(function(d, i) { return i ? 0 : -2000; })
+        .nodes(nodes)
+        .size([width, height]);
+
+    force.start();
+
+    var svg = d3.select("#dotViz").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    svg.selectAll("circle")
+        .data(nodes.slice(1))
+      .enter().append("circle")
+        .attr("r", function(d) { return d.radius; })
+        .style("fill", function(d, i) { return color(i % 3); });
+
+    force.on("tick", function(e) {
+      var q = d3.geom.quadtree(nodes),
+          i = 0,
+          n = nodes.length;
+
+      while (++i < n) q.visit(collide(nodes[i]));
+
+      svg.selectAll("circle")
+          .attr("cx", function(d) { return d.x; })
+          .attr("cy", function(d) { return d.y; });
+    });
+
+    svg.on("mousemove", function() {
+      var p1 = d3.mouse(this);
+      root.px = p1[0];
+      root.py = p1[1];
+      force.resume();
+    });
+
+    function collide(node) {
+      var r = node.radius + 16,
+          nx1 = node.x - r,
+          nx2 = node.x + r,
+          ny1 = node.y - r,
+          ny2 = node.y + r;
+      return function(quad, x1, y1, x2, y2) {
+        if (quad.point && (quad.point !== node)) {
+          var x = node.x - quad.point.x,
+              y = node.y - quad.point.y,
+              l = Math.sqrt(x * x + y * y),
+              r = node.radius + quad.point.radius;
+          if (l < r) {
+            l = (l - r) / l * .5;
+            node.x -= x *= l;
+            node.y -= y *= l;
+            quad.point.x += x;
+            quad.point.y += y;
+          }
         }
+        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+      };
+    }
 });
