d181c771fdbd6a077169d7bd95688596d5e47a4c
Ian Macalinao <ian@giza.us>
2014-09-21 04:21:25 -0500

    add vis digest handler

--- public/vis.js
+++ public/vis.js
@@ -1,7 +1,14 @@
 var initVised = false;
 
-function setupVis() {
-  var width = '100%';
+var colors = ['#AE81FF', '#E6DB74', '#66D9EF', '#465457'];
+
+function initVis() {
+  if (initVised) {
+    return;
+  }
+
+  // init
+  var width = 750;
   var height = 500;
 
   var i = 0;
@@ -14,34 +21,43 @@ function setupVis() {
     .attr('width', width)
     .attr('height', height);
 
-  setInterval(function() {
+  function makeCircle(opts) {
     var circle = svg.insert('circle', 'rect')
-      .attr('cx', 200)
-      .attr('cy', height / 2)
+      .attr('cx', opts.x * width)
+      .attr('cy', opts.y * height)
       .attr('r', 1e-6)
-      .style('stroke', d3.rgb(0, 0, 0))
+      .style('stroke', opts.color)
       .style('stroke-opacity', 1);
 
     // Faded
     circle.transition()
-      .duration(2000)
+      .duration(opts.duration)
       .ease(Math.sqrt)
       .attr('r', 100)
       .style('stroke-opacity', 1e-6)
       .remove();
-  }, 1000);
-}
-
-function digestHandler() {
-
-}
-
-function initVis() {
-  if (initVised) {
-    return;
   }
-  console.log('initvis');
-  setupVis();
+
+  var circles = {};
+
+  function digestHandler(digest) {
+    _.forEach(digest, function(val, id) {
+      var circle = circles[id];
+      if (!circle) {
+        circle = {
+          x: Math.random() * 0.8 + 0.1,
+          y: Math.random() * 0.8 + 0.1,
+          color: colors[Math.floor(Math.random() * colors.length)]
+        };
+      }
+
+      var opts = _.clone(circle);
+      opts.duration = 1000;
+
+      makeCircle(opts);
+    });
+  }
+
   socket.on('activityDigest', digestHandler);
   initVised = true;
 }
